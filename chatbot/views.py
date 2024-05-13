5a0d825260240447c53051cc15c0d7828c6886d9
M-Adil-Sultan <m.adil.sultan.as@gmail.com>
2024-05-13 14:55:00 +0500

    decresed chunk size

--- chatbot/views.py
+++ chatbot/views.py
@@ -34,7 +34,7 @@ def initialize_chain():
     # Get the data from the database
     queryset = Train_dataset.objects.all()
     qa_data = queryset.values('question', 'answers')
-    # print("qa_data:", qa_data)
+    # print(f"--------qa_data: {qa_data} ---------")
     
     # Convert the data into a DataFrame
     df = pd.DataFrame.from_records(qa_data)
@@ -64,7 +64,7 @@ def initialize_chain():
     #     metadata = document.metadata
 
     # Initialize the text splitter and embeddings
-    text_splitter = CharacterTextSplitter(chunk_size=1200, chunk_overlap=200)
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
     texts = text_splitter.split_documents(documents)
     embeddings = OpenAIEmbeddings()
 
@@ -79,6 +79,7 @@ def initialize_chain():
         llm=ChatOpenAI(model="gpt-4"),
         retriever=docsearch.as_retriever(search_kwargs={"k": 1}),
     )
+    
 
     return chain
 
@@ -91,6 +92,7 @@ def chain_initializer(request):
     return redirect('chatbot')
 
 def chat_with_langchain(query, chat_history):
+    chain({"question": query, "chat_history": chat_history})
     result = chain({"question": query, "chat_history": chat_history})
     answer = result['answer']
     chat_history.append((query, answer))
@@ -113,12 +115,14 @@ def chatbot(request):
         if db_response is not None and db_response.answers is not None:
             chat = Chat(user=request.user, message=message, response=db_response.answers, created_at=timezone.now())
             chat.save()
+            print(f"I came from db {db_response.answers} :--------------------------------")
             return JsonResponse({'message': message, 'response': db_response.answers})
 
         response = ask_openai(message)
 
         chat = Chat(user=request.user, message=message, response=response, created_at=timezone.now())
         chat.save()
+        print(f"----------------I came from Openai  {response}")
         return JsonResponse({'message': message, 'response': response})
     dict = {}
     for chat in chats:
